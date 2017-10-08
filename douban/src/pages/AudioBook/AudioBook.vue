c426ca38fb29475d7198bac9e87bee8995acaede
lossp <richard.xxmxx@gmail.com>
2017-10-08 20:27:37 +0800

    update

--- douban/src/pages/AudioBook/AudioBook.vue
+++ douban/src/pages/AudioBook/AudioBook.vue
@@ -45,9 +45,10 @@
             })
         },
         methods:{
+          
             fetchBook(){
                 const Vue = this;
-                let url = `api/book/search?q=JavaScript&fields=all&count=${Vue.count}&start=${Vue.start}`;
+                let url = `https://api.douban.com/v2/book/search?q=JavaScript&fields=all&count=${Vue.count}&start=${Vue.start}`;
                 this.axios.get(url).then((response) => {
                     console.log(response)
                 let data = response.data.books
