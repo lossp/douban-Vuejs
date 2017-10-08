78ad193fa9f5dc49d1377594fc9c2efedf0ad64a
lossp <richard.xxmxx@gmail.com>
2017-10-08 15:25:15 +0800

    updated

--- douban/config/index.js
+++ douban/config/index.js
@@ -19,16 +19,7 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report,
-    proxyTable: {
-      '/api': {
-          target: 'https://api.douban.com/v2',
-          changeOrigin: true,
-          pathRewrite: {
-              '^/api': ''
-          }
-      }
-    }
+    bundleAnalyzerReport: process.env.npm_config_report
   },
   dev: {
     env: require('./dev.env'),
