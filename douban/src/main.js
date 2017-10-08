b170a5645ca1cfbd9fe1662510480de6ac2282ab
lossp <richard.xxmxx@gmail.com>
2017-10-08 15:09:39 +0800

    update

--- douban/src/main.js
+++ douban/src/main.js
@@ -14,24 +14,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App },
-  watch:{
-    "$router":"checkLogin"
-  },
-  methods:{
-    checkLogin(){
-      //检查是否存在session
-      //cookie操作方法在源码里有或者参考网上的即可
-      if(!this.getCookie('session')){
-        //如果没有登录状态则跳转到登录页
-        this.$router.push('/login');
-      }else{
-        //否则跳转到登录后的页面
-        this.$router.push('/user_info');
-      }
-    }
-  },
-  created(){
-    this.checkLogin();
-  }
+  components: { App }
 })
